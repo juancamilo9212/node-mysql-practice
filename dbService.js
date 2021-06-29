21c3a0562ad3d6434d00b8d974b59ced07ae5416
Juan Camargo <juan.camargo@moove-it.com>
2021-06-29 18:35:59 -0500

    Practica NodeJS,Express y mysql

---
+++ dbService.js
@@ -0,0 +1,108 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
+let instance = null;
+/*const connection = mysql.createConnection({
+host:process.env.HOST,
+user:process.env.USER,
+password:process.env.PASSWORD,
+database:process.env.DATABASE,
+port:process.env.DBPORT
+});*/
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Mecatronica9212',
+    database:'web_app',
+    port:'3306'
+});
+
+connection.connect((err) => {
+if(err){
+    console.log(err.message);
+}else{
+    console.log('DB Connected!');
+}
+});
+
+class DbService{
+    static getDbServiceInstance(){
+        return instance ? instance:new DbService;
+    }
+
+    async getAllData() {
+        try {
+            const response = await new Promise((resolve,reject) => {
+                const query = 'SELECT * FROM Names;';
+                connection.query(query,(error,results) => {
+                    if(error) reject(new Error(error.message));
+                    resolve(results);
+                });
+            });
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async createName(nombre){
+        try {
+            const date = new Date();
+            const insertId = await new Promise((resolve,reject) => {
+                const query = 'INSERT INTO Names (nombre,date) VALUES (?,?);';
+                connection.query(query,[nombre,date], (error, result) => {
+                    if(error) reject(new Error(error.message));
+                    resolve(result.insertId);
+                });
+            });
+            return {
+                id: insertId,
+                nombre:nombre,
+                date:date
+            };
+        } catch (error) {
+            console.log(error);
+        }   
+    }
+
+    async deleteRow(id){
+        id = parseInt(id,10);
+        try {
+            const response = await new Promise((resolve,reject) => {
+                const query = 'DELETE FROM Names WHERE id = ?;';
+                connection.query(query,[id], (error, result) => {
+                    if(error) reject(new Error(error.message));
+                    resolve(result);
+                });
+            });
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async updateNameById(id,nombre){
+        id = parseInt(id,10);
+        const date = new Date();
+        try {
+            const response = await new Promise((resolve,reject) => {
+                const query = 'UPDATE Names SET nombre=?,date=? WHERE id=? ;';
+                connection.query(query,[nombre,date,id], (error, result) => {
+                    if(error) reject(new Error(error.message));
+                    resolve(result);
+                });
+            });
+            return {
+                id:id,
+                nombre:nombre,
+                date:date
+            }
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+}
+
+module.exports=DbService;
---
+++ index.js
@@ -0,0 +1,55 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+const dbService = require('./dbService');
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.post('/insert',(request,response) => {
+    const {nombre} = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.createName(nombre);
+    result.then(data => response.json({success:true}))
+    .catch(err => console.log(err));
+});
+
+app.get('/getAll',(request,response) => {
+    const db = dbService.getDbServiceInstance();
+    const result=db.getAllData();
+    result.then(data => 
+        response.json(
+            {
+                data:data
+            })).catch(err => {
+                console.log(err);
+            })
+});
+
+app.delete('/remove/:id',(request,response) => {
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
+    const result = db.deleteRow(id);
+    result.then(data => {
+        response.json({success:true})
+    }).catch(err => console.log(err));
+});
+
+app.put('/updateName/:id',(request,response) => {
+    const {id} = request.params;
+    const {nombre} = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.updateNameById(id,nombre); 
+    result.then(data => 
+        response.json(
+            {
+                data:data
+            })).catch(err => {
+                console.log(err);
+            })
+});
+
+app.listen(process.env.PORT, () => console.log('app is running'));
